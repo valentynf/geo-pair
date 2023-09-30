8686b6925a0c5033fd99fbaf30a4375afe562791
valentynf <valentynf@wix.com>
2023-09-30 13:24:55 +0300

    replaced fisher yates algo with sort solution

--- src/components/GeoPairGame.tsx
+++ src/components/GeoPairGame.tsx
@@ -4,23 +4,14 @@ type GamePropsType = {
   data: { [country: string]: string };
 };
 
-function fisherYatesShuffle(arr: string[]) {
-  return arr.reduce(
-    (res, _, i) => {
-      const randIndex = Math.floor(Math.random() * (i + 1));
-      [res[i], res[randIndex]] = [res[randIndex], res[i]];
-      return res;
-    },
-    [...arr]
-  );
-}
+const shuffleArray = (array: string[]) => array.sort(() => Math.random() - 0.5);
 
 function GeoPairGame({ data }: GamePropsType) {
   const [countries, capitals] = Object.entries(data);
 
   return (
     <div id="game">
-      {fisherYatesShuffle([...countries, ...capitals]).map((name, index) => (
+      {shuffleArray([...countries, ...capitals]).map((name, index) => (
         <GeoButton key={index} name={name} />
       ))}
     </div>
